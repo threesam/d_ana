7796624d962c8aa3dadf35d50b74582458fe0a3d
Sam <57677512+threesam@users.noreply.github.com>
2023-06-04 15:27:20 -0400

    updates hr

--- src/routes/thoughts/[handle]/+page.svelte
+++ src/routes/thoughts/[handle]/+page.svelte
@@ -4,11 +4,13 @@
 	export let data;
 </script>
 
-<section class="grid grid-cols-1 lg:grid-cols-2">
+<section class="grid grid-cols-1 border-b border-dark lg:grid-cols-2">
 	<img src={data.post.image.asset.url} alt={data.post.title} />
 	<h1 class="px-5 py-5 text-xl font-semibold lg:grid lg:place-content-center lg:px-10 lg:text-3xl">
 		{data.post.title}
 	</h1>
 </section>
-<hr />
-<PortableText blocks={data.post.body} />
+
+<section class="p-5 pb-0 lg:p-10">
+	<PortableText blocks={data.post.body} />
+</section>
